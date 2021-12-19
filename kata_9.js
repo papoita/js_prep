87b19ffa581b7e3f75aad610b752202edebda3ce
PaolaP <paolaperez8@gmail.com>
2021-12-18 21:29:40 -0500

    kata 9 prep stretch

---
+++ kata_9.js
@@ -0,0 +1,9 @@
+const camelCase = function(input) {
+  // Your code here
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
+
+//toUpperCase only first letter of word replace space with 

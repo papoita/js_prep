f3e4d2e13f3f8e86bf79744d43abe9c25d5556f5
PaolaP <paolaperez8@gmail.com>
2021-12-18 21:31:55 -0500

    kata 10 prep strech

---
+++ kata_10.js
@@ -0,0 +1,7 @@
+const multiplicationTable = function(maxValue) {
+  // Your code here
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
